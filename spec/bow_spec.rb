cb01714f58ce2988377572643b269ad85ed0d168
cjvirtucio87 <cjv287@gmail.com>
2016-07-11 11:58:06 -0700

    Working in viking spec.

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -1,1 +1,32 @@
-# Your code here
+require_relative '../lib/weapons/bow'
+
+describe Bow do
+  describe "#arrows" do
+    it "returns a fixed number" do
+      expect(Bow.new.arrows).to be_an_instance_of(Fixnum)
+    end
+
+    it "returns 10 as the default arrow count" do
+      bow = Bow.new
+      expect(Bow.new.arrows).to eq(10)
+    end
+
+    it "returns the specified number of arrows" do
+      expect(Bow.new(32132).arrows).to eq(32132)
+    end
+  end
+
+  describe "#use" do
+    it "reduces the arrow count by 1" do
+      bow = Bow.new
+      bow.use
+      expect(bow.arrows).to eq(9)
+    end
+
+    it "cannot be called when arrow count is zero" do
+      bow = Bow.new(0)
+      expect{ bow.use }.to raise_error("Out of arrows")
+    end
+
+  end
+end
--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -1,1 +1,11 @@
-# Your code here
+require_relative '../lib/weapons/viking'
+
+describe Viking do
+
+  describe "#initialize" do
+    it "sets the viking's name to the argument passed to it" do
+      # expect(Viking.new("Vik").).to eq("Vik")
+    end
+  end
+
+end
