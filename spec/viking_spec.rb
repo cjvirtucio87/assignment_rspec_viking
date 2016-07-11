7994f863297db34c3c725eb9e11b3a94eeaa21be
cjvirtucio87 <cjv287@gmail.com>
2016-07-11 14:33:01 -0700

    Test for strength and multi.

--- spec/viking_spec.rb
+++ spec/viking_spec.rb
@@ -5,6 +5,7 @@ describe Viking do
   describe "#initialize" do
     let(:axe){ Axe.new }
     let(:bow){ Bow.new }
+    let(:vik){ Viking.new("Vik") }
 
     it "sets the viking's name to the argument passed to it" do
       expect(Viking.new("Vik").name).to eq("Vik")
@@ -23,10 +24,11 @@ describe Viking do
         .to raise_error(NoMethodError)
     end
 
-     it "doesn't have a weapon when first initialized" do
-       expect(Viking.new("Vik", 200).weapon).to eq(nil)
-     end
+    it "doesn't have a weapon when first initialized" do
+      expect(Viking.new("Vik", 200).weapon).to eq(nil)
+    end
 
+  describe "#pick_up_weapon" do
      it "gives the viking a weapon" do
        expect(Viking.new("Vik").pick_up_weapon(axe)).to eq(axe)
      end
@@ -40,7 +42,54 @@ describe Viking do
        expect{ Viking.new("Vik").pick_up_weapon("axe") }
          .to raise_error("Can't pick up that thing") 
      end
-
   end
 
+  describe "#drop_weapon" do 
+    it "dropping a viking's weapon leaves him weaponless" do
+      vik.pick_up_weapon(axe)
+      vik.drop_weapon
+      expect(vik.weapon).to eq(nil)
+    end
+  end
+
+  describe "#receive_attack" do
+    it "takes a fixed number and changes the viking's health by that number" do
+      vik.receive_attack(10)
+      expect(vik.health).to eq(90)
+    end
+
+    it "calls the take_damage method" do
+      expect(vik).to receive(:take_damage).with(10).and_return(vik.health)
+      vik.receive_attack(10)
+    end
+  end
+
+  describe "#attack" do
+    let(:enemy) { Viking.new("Enemy") }
+    it "causes the recipient's health to drop" do
+      vik.pick_up_weapon(bow)
+      vik.attack(enemy)
+      expect(enemy.health).to eq(80)
+    end
+
+    it "calls the recipient's take_damage method" do
+      expect(enemy).to receive(:take_damage).with(2.5).and_return(enemy.health)
+      vik.attack(enemy)
+    end
+
+    it "runs damage with fists when the viking has no weapon" do
+      vik.attack(enemy)
+      expect(enemy.health).to eq(97.5)
+    end
+
+    it "reduces the enemy's health by the weapon's 
+        multiplier times the attacker's strength" do
+      multi = Fists.new.use
+      strength = vik.strength
+      vik.attack(enemy)
+      expect(100-enemy.health).to eq(strength * multi)
+    end
+  end
+
+end
 end
